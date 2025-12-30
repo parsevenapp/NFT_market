15368fbbcfc4f0303a7ee5f3a7d3f5576036bc0b
Parsapp <malifaranak@gmail.com>
2025-12-30 21:28:02 +0330

    Add ThirdwebProvider to MyApp component

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider 
+      activeChain="polygon" 
+      clientId="A98083f883a97f19e073829656d7fd42"
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
+
