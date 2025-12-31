d83869afd4ccc836c23b22d9228b711ae13f0fb2
Parsapp <malifaranak@gmail.com>
2025-12-31 18:57:30 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Polygon, Base } from "@thirdweb-dev/chains";
+import { Polygon } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
-
-// Your Client ID
-const CLIENT_ID = "a98083f883a97f19e073829656d7fd42";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider 
       activeChain={Polygon} 
-      supportedChains={[Polygon, Base]}
-      clientId={CLIENT_ID}
+      clientId="a98083f883a97f19e073829656d7fd42"
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
