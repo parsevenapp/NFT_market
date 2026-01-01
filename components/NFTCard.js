0eaad1933673521775c8361fa35a7c3c72a59ae3
Parsapp <malifaranak@gmail.com>
2026-01-01 19:17:35 +0330

    Update NFTCard.js

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,31 +1,27 @@
-import styles from "../styles/Home.module.css";
+import { Web3Button } from "@thirdweb-dev/react";
+import { MARKETPLACE_ADDRESS } from "../const/contractAddresses";
 
 export default function NFTCard({ nft }) {
   return (
     <div className="nft-card">
-      <img 
-        src={nft.metadata.image} 
-        alt={nft.metadata.name} 
-        className="nft-image" 
-      />
+      {/* در مارکت‌پلیس v3، اطلاعات در asset قرار دارد */}
+      <img src={nft.asset.image} alt={nft.asset.name} className="nft-image" />
       <div style={{ padding: "10px 0" }}>
-        <h3 style={{ margin: "5px 0", fontSize: "1.1rem" }}>{nft.metadata.name}</h3>
-        <p style={{ color: "#888", fontSize: "0.8rem" }}>Token ID: #{nft.metadata.id}</p>
-        
-        {/* این دکمه در مرحله ۳.۳ به تابع خرید وصل می‌شود */}
-        <button style={{
-          width: "100%",
-          marginTop: "15px",
-          padding: "10px",
-          borderRadius: "8px",
-          border: "none",
-          backgroundColor: "#fff",
-          color: "#000",
-          fontWeight: "bold",
-          cursor: "pointer"
-        }}>
+        <h3 style={{ margin: "5px 0" }}>{nft.asset.name}</h3>
+        <p style={{ color: "#00ffad", fontWeight: "bold" }}>
+          Price: {nft.currencyValuePerToken.displayValue} {nft.currencyValuePerToken.symbol}
+        </p>
+
+        <Web3Button
+          contractAddress={MARKETPLACE_ADDRESS}
+          // تابع مخصوص خرید در Marketplace V3
+          action={(contract) => contract.directListings.buyFromListing(nft.id, 1)}
+          onSuccess={() => alert("خرید با موفقیت انجام شد! NFT به ولت شما منتقل شد.")}
+          onError={(err) => alert("خطا در خرید: " + err.message)}
+          className="buy-button"
+        >
           Buy Now
-        </button>
+        </Web3Button>
       </div>
     </div>
   );
