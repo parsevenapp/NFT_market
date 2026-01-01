eb840147fdc2adff13cb6d79b1e8eb9a293b04ce
Parsapp <malifaranak@gmail.com>
2026-01-01 20:15:04 +0330

    Update NFTCard.js

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,27 +1,33 @@
-import { Web3Button } from "@thirdweb-dev/react";
+import { MediaRenderer, useContract, useDirectListing } from "@thirdweb-dev/react";
 import { MARKETPLACE_ADDRESS } from "../const/contractAddresses";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function NFTCard({ nft }) {
-  return (
-    <div className="nft-card">
-      {/* در مارکت‌پلیس v3، اطلاعات در asset قرار دارد */}
-      <img src={nft.asset.image} alt={nft.asset.name} className="nft-image" />
-      <div style={{ padding: "10px 0" }}>
-        <h3 style={{ margin: "5px 0" }}>{nft.asset.name}</h3>
-        <p style={{ color: "#00ffad", fontWeight: "bold" }}>
-          Price: {nft.currencyValuePerToken.displayValue} {nft.currencyValuePerToken.symbol}
-        </p>
+  const { contract } = useContract(MARKETPLACE_ADDRESS, "marketplace-v3");
 
-        <Web3Button
-          contractAddress={MARKETPLACE_ADDRESS}
-          // تابع مخصوص خرید در Marketplace V3
-          action={(contract) => contract.directListings.buyFromListing(nft.id, 1)}
-          onSuccess={() => alert("خرید با موفقیت انجام شد! NFT به ولت شما منتقل شد.")}
-          onError={(err) => alert("خطا در خرید: " + err.message)}
-          className="buy-button"
-        >
-          Buy Now
-        </Web3Button>
+  return (
+    <div className="nft-card" style={{ padding: "15px" }}>
+      <MediaRenderer src={nft.metadata.image} className="nft-image" />
+      
+      <div style={{ marginTop: "15px" }}>
+        <h3 style={{ margin: "0 0 5px 0" }}>{nft.metadata.name}</h3>
+        <p style={{ color: "#888", fontSize: "0.8rem" }}>Token ID: #{nft.metadata.id}</p>
+        
+        <Link href={`/nft/${nft.owner}/${nft.metadata.id}`}>
+          <button style={{
+            width: "100%",
+            padding: "10px",
+            marginTop: "10px",
+            backgroundColor: "#00ffad",
+            border: "none",
+            borderRadius: "8px",
+            fontWeight: "bold",
+            color: "#000"
+          }}>
+            View Details
+          </button>
+        </Link>
       </div>
     </div>
   );
